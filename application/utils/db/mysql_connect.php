1e16d735bfb0c4f7983935485f44019e39fa7f3b
David Graf <davidagraf@gmail.com>
2011-06-20 16:42:26 +0200

    Correct error msgs in mysql_connect.php.

--- application/utils/db/mysql_connect.php
+++ application/utils/db/mysql_connect.php
@@ -5,7 +5,13 @@ $db_username = "tables";
 $db_pass = "tables";
 $db_name = "tables";
 
-@mysql_connect("$db_host","$db_username","$db_pass") or die ("Could not connect to MySQL.");
-@mysql_select_db("$db_name") or die ("No database");
+if (!@mysql_connect("$db_host","$db_username","$db_pass")) {
+  header('HTTP/1.1 500 Internal Server Error');
+  die ("Could not connect to MySQL.");
+}
+if (!@mysql_select_db("$db_name")) {
+  header('HTTP/1.1 500 Internal Server Error');
+  die ("No database");
+}
 
 ?>
