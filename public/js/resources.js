0bc2258ba56a1abd0dec035f9272fa4659d34af3
David Graf <davidagraf@gmail.com>
2011-06-24 14:27:12 +0200

    Using the new config file in javascript correctly.

--- public/js/resources.js
+++ public/js/resources.js
@@ -1,11 +1,13 @@
 /**
  * Resources configuration
+ * Needs to be imported in the html page first to set globalResources before doing anything.
  */
 var globalResources = {};
 
 $.ajax( {
   url : "/public/js/resources.json",
   dataType : 'json',
+  async : false,
   success : function(data, textStatus, xhr) {
     globalResources = data;
   },
